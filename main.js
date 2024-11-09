021f3004a0490621abf5e82b5251b86e4e14c72f
Judy21fei bc2024-5 <yuliana.zimirska@lnu.edu.ua>
2024-11-09 18:48:16 +0200

    First part

---
+++ main.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const http = require('http');
+const commander = require('commander');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+commander
+  .requiredOption('-h, --host <host>', 'Server host')
+  .requiredOption('-p, --port <port>', 'Server port')
+  .requiredOption('-c, --cache <cacheDir>', 'Cache directory path')
+  .parse(process.argv);
+
+const { host, port, cache } = commander.opts();
+
+
+if (!fs.existsSync(cache)) {
+  fs.mkdirSync(cache);
+}
