16d3c83e313c36c4d5f1dc12dced2b7ed74c5874
Judy21fei bc2024-5 <yuliana.zimirska@lnu.edu.ua>
2024-11-09 21:07:12 +0200

    Added post, put etc

--- main.js
+++ main.js
@@ -14,3 +14,57 @@ const { host, port, cache } = program.opts();
 app.listen(port, host, () => {
   console.log(`Server running at http://${host}:${port}`);
 });
+
+const notes = {};
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/notes/:name', (req, res) => {
+  const { name } = req.params;
+  if (notes[name]) {
+    res.send(notes[name]);
+  } else {
+    res.status(404).send('Note not found');
+  }
+});
+
+app.put('/notes/:name', (req, res) => {
+  const { name } = req.params;
+  const { text } = req.body;
+  if (notes[name]) {
+    notes[name] = text;
+    res.send('Note updated');
+  } else {
+    res.status(404).send('Note not found');
+  }
+});
+
+app.delete('/notes/:name', (req, res) => {
+  const { name } = req.params;
+  if (notes[name]) {
+    delete notes[name];
+    res.send('Note deleted');
+  } else {
+    res.status(404).send('Note not found');
+  }
+});
+
+app.get('/notes', (req, res) => {
+  const noteList = Object.keys(notes).map(name => ({ name, text: notes[name] }));
+  res.json(noteList);
+});
+
+app.post('/write', (req, res) => {
+  const { note_name, note } = req.body;
+  if (notes[note_name]) {
+    res.status(400).send('Note already exists');
+  } else {
+    notes[note_name] = note;
+    res.status(201).send('Note created');
+  }
+});
+
+app.get('/UploadForm.html', (req, res) => {
+  res.sendFile(__dirname + '/UploadForm.html');
+});
